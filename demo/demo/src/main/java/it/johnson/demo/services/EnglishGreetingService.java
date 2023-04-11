f00c91572e3294f7f6c42a2cb7c0acc71f3d7b08
MukengeNJ <john.mukenge>
2023-04-11 12:14:42 +0200

    the default profile is active when no active profiles are available

--- demo/demo/src/main/java/it/johnson/demo/services/EnglishGreetingService.java
+++ demo/demo/src/main/java/it/johnson/demo/services/EnglishGreetingService.java
@@ -3,7 +3,7 @@ package it.johnson.demo.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("EN")
+@Profile({"EN", "default"})
 @Service("i18nService")
 public class EnglishGreetingService implements GreetingService{
     @Override
