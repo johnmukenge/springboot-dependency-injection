4f31749256d05d178bd01fd14ac94845fd902ca6
MukengeNJ <john.mukenge>
2023-04-11 11:37:44 +0200

    primary bean: in autowired scenario, whwn adding a stereotype @Primary over a bean will tell spring to prefer (instanziate)this one over the other

---
+++ demo/demo/src/main/java/it/johnson/demo/services/GreetingServicePrimary.java
@@ -0,0 +1,13 @@
+package it.johnson.demo.services;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+@Primary
+@Service
+public class GreetingServicePrimary implements GreetingService{
+    @Override
+    public String sayGreeting() {
+        return "Hello From the primary bean!!";
+    }
+}
